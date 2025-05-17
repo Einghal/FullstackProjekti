da125f8acbe4c5af0367c7bb91d423783f19bff0
Einghal <146416502+Einghal@users.noreply.github.com>
2025-05-17 16:55:26 +0300

    kokeilu

--- nextjs-dashboard/app/seed/route.ts
+++ nextjs-dashboard/app/seed/route.ts
@@ -47,7 +47,7 @@ async function seedInvoices() {
       (invoice) => sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
+        ON CONFLICT DO NOTHING;
       `,
     ),
   );
